5d7edc0d15a4d3d0b754425f2c3b1c9e62d95bff
JaCa <2628685929@qq.com>
2017-04-05 21:40:46 +0800

    home.index change.

--- resources/views/frontend/home/index.blade.php
+++ resources/views/frontend/home/index.blade.php
@@ -6,10 +6,10 @@
     <div class="container home_bg">
         <div class="row">
             <div class="col-sm-12">
-                <div class="center" style="position: absolute;width: 300px;bottom: 30%;left: 50%;margin-left: -150px;">
-                    <label>{{ isset($fromuser) ? $fromuser : '' }}</label>邀請你來測試
+                <div class="center" style="position: absolute;width: 300px;bottom:50px;left: 50%;margin-left: -150px;">
+                    <p class="text-center fromuser">{{ isset($fromuser) ? $fromuser : '' }}aa</p>
                     <p class="text-center">你們到底適不適合一起旅行</p>
-                    <a class="btn btn-primary" href="#">設置我的旅行偏好</a>
+                    <a class="btn btn-primary center-block" href="#">設置我的旅行偏好</a>
                 </div>
             </div>
         </div>
