99a8a91485020b0732810b51ff1f84e065f4c3cc
JaCa <2628685929@qq.com>
2017-04-06 00:20:48 +0800

    home.index change.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,18 +2,24 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 
-use App\Http\Requests;
+use EasyWeChat\Foundation\Application;
 
 class HomeController extends Controller
 {
     //Home
     public function index() {
         $user = session('wechat.oauth_user');
+        $wechat = app('wechat');
+        $js = $wechat->js;
         if ($user) {
-            return view('frontend.home.index')->with('fromuser',$user->getNickname());
+            return view('frontend.home.index')->with('fromuser',$user->getNickname())->with('js',$js);
         }
         return view('frontend.home.index');
+    }
+
+    public function demo(Application $wechat)
+    {
+        // $wechat 则为容器中 EasyWeChat\Foundation\Application 的实例
     }
 }
