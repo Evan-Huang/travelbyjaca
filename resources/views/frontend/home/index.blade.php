9c0289d2f1d129d9cf2f5ce521452de84936170f
JaCa <2628685929@qq.com>
2017-04-06 01:12:48 +0800

    home.index change share.

--- resources/views/frontend/home/index.blade.php
+++ resources/views/frontend/home/index.blade.php
@@ -32,9 +32,11 @@
             dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
             success: function () {
                 // 用户确认分享后执行的回调函数
+                alert('分享成功！！！')
             },
             cancel: function () {
                 // 用户取消分享后执行的回调函数
+                alert('分享失敗！！！')
             }
         });
     </script>
